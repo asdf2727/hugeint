8752e4439200e7e42a2b70a4e329f47008497898
asdf2727 <54817105+asdf2727@users.noreply.github.com>
2022-05-21 14:32:32 +0300

    Cleaned up the example main file.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 #include <chrono>
-#include <map>
 
 #include "hugeint.h"
 
@@ -24,12 +22,6 @@ public:
 	}
 };
 
-struct str {
-	uint val;
-	uint pow;
-};
-//Decomp fact is a very cool idea and briliant implementation (by me)...
-//usless because its to slow doe
 hugeint logPow (hugeint base, uint pow) {
 	hugeint ans = 1;
 	while (pow > 0) {
@@ -41,58 +33,16 @@ hugeint logPow (hugeint base, uint pow) {
 	}
 	return ans;
 }
-hugeint decompFact (uint fact) {
-	std::vector <bool> iscomp;
-	std::vector <str> vec;
-	uint copy;
-	hugeint ans = 1;
-	iscomp.resize(fact + 1);
-	for (uint index = 2; index * index <= fact; index++) {
-		if (!iscomp[index]) {
-			for (uint index2 = index * index; index2 * index2 <= fact; index2 += index) {
-				iscomp[index2] = true;
-			}
-			vec.push_back({ index, 0 });
-		}
-	}
-	for (uint index = 2; index <= fact; index++) {
-		copy = index;
-		for (str &prim : vec) {
-			while (copy % prim.val == 0) {
-				prim.pow++;
-				copy /= prim.val;
-			}
-			if (copy == 1) {
-				break;
-			}
-		}
-		if (copy > 1) {
-			ans *= copy;
-		}
-	}
-	for (str now : vec) {
-		ans *= logPow(now.val, now.pow);
-	}
-	return ans;
-}
-
-hugeint normFact (uint fact) {
-	hugeint ans = 1;
-	for (uint index = 2; index <= fact; index++) {
-		ans *= index;
-	}
-	return ans;
-}
-
-timer global;
 
 int main () {
 	std::ofstream fout("file.txt");
-	hugeint nr1, nr2;
+	timer global;
+	hugeint nr1;
+	uint nr2;
 	std::cin >> nr1 >> nr2;
 	global.reset();
-	hugeint ans = logPow(nr1, (uint)nr2);
+	hugeint ans = logPow(nr1, nr2);
 	std::cout << global.reset() << '\n';
-	std::cout << ans << '\n';
+	fout << ans << '\n';
 	std::cout << global.reset();
 }
